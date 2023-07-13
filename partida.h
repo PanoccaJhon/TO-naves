45c1fd5e3172b8c1484679853194bd3487ff8626
jhon panocca <jpanoccam@unsa.edu.pe>
2023-07-13 15:40:48 -0500

    implementacion simple de partida.h

--- partida.h
+++ partida.h
@@ -1,18 +1,22 @@
 #ifndef PARTIDA_H
 #define PARTIDA_H
+#include "avion.h"
 
 #include <SFML/Graphics.hpp>
 class Partida
 {
 public:
     Partida();
+    void configurar();
+    void enJuego();
 
 private:
     sf::RenderWindow window;
     bool estado;
     int puntaje;
     int nivel;
-
+    Avion avion;
+    //Agregar mas cosas
 };
 
 #endif // PARTIDA_H
