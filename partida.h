4357c4daf9a1ae07264b767b61c1f8cb01ff8b20
jhon panocca <jpanoccam@unsa.edu.pe>
2023-07-13 15:32:46 -0500

    agregado clase avion

--- partida.h
+++ partida.h
@@ -1,11 +1,18 @@
 #ifndef PARTIDA_H
 #define PARTIDA_H
 
-
+#include <SFML/Graphics.hpp>
 class Partida
 {
 public:
     Partida();
+
+private:
+    sf::RenderWindow window;
+    bool estado;
+    int puntaje;
+    int nivel;
+
 };
 
 #endif // PARTIDA_H
